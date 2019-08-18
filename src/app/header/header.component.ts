5ea35b1655a3a8156e9cd6b158d838b3548db4b9
Chris Bell <chrislbellny@gmail.com>
2019-08-18 13:10:22 -0500

    working user settings modal

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
@@ -8,11 +9,22 @@ import { AuthService } from '../auth/auth.service';
 })
 export class HeaderComponent implements OnInit {
   username: string;
+  userSettingsForm: FormGroup;
 
-  constructor(private readonly authService: AuthService) { }
+  constructor(
+    private readonly authService: AuthService,
+    private readonly formBuilder: FormBuilder,
+  ) { }
 
   ngOnInit() {
     this.username = this.authService.getUser();
+    this.userSettingsForm = this.formBuilder.group({
+      username: [this.username]
+    });
+  }
+
+  onSubmit() {
+    this.username = this.userSettingsForm.controls.username.value;
   }
 
 }
